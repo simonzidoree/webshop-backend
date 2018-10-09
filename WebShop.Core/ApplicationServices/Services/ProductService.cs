f072e4f678cb092ad40b5a5bee53e20bf5e9310f
Riis18 <Riisjesper@hotmail.com>
2018-10-09 10:17:32 +0200

    Added methods from interfaces

--- WebShop.Core/ApplicationServices/Services/ProductService.cs
+++ WebShop.Core/ApplicationServices/Services/ProductService.cs
@@ -1,10 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using WebShop.Core.Entities;
 
 namespace WebShop.Core.ApplicationServices.Services
 {
-    class ProductService
+    class ProductService : IProductService
     {
+        public Product CreateProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product DeleteProduct(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product FindProductById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetAllProducts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product NewProduct(string name, double price, string description, int stock, string imageURL)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product UpdateProduct(int id, Product productUpdate)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- WebShop.Infrastructure.Data/Repositories/ProductRepository.cs
+++ WebShop.Infrastructure.Data/Repositories/ProductRepository.cs
@@ -1,10 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using WebShop.Core.DomainServices;
+using WebShop.Core.Entities;
 
 namespace WebShop.Infrastructure.Data.Repositories
 {
-    class ProductRepository
+    public class ProductRepository : IProductRepository
     {
+        public Product Create(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Product> ReadAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product ReadById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product Update(Product productUpdate)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
