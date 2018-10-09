5128eeac07854ec4eda56923a6ff8f328bc271fb
Simon Zidoree <simzo96@hotmail.com>
2018-10-09 09:40:33 +0200

    Made ProductService and ProductRepository interfaces.

--- WebShop.Core/ApplicationServices/IProductService.cs
+++ WebShop.Core/ApplicationServices/IProductService.cs
@@ -1,10 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
+using WebShop.Core.Entities;
 
 namespace WebShop.Core.ApplicationServices
 {
-    interface IProductService
+    public interface IProductService
     {
+        //New Product
+        Product NewProduct(string name,
+            double price,
+            string description,
+            int stock,
+            string imageURL);
+
+        //Create
+        Product CreateProduct(Product product);
+
+        //Read
+        Product FindProductById(int id);
+
+        List<Product> GetAllProducts();
+
+        //Update
+        Product UpdateProduct(int id, Product productUpdate);
+
+        //Delete
+        Product DeleteProduct(int id);
     }
 }
